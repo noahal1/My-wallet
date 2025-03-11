01c0007605b35b6d5ca99217659faa27be960c5a
noahal1 <noahall127@outlook.com>
2025-03-11 15:40:38 +0800

    first commit

---
+++ src/main.js
@@ -0,0 +1,14 @@
+import { createApp } from 'vue'
+import App from '@/App.vue'
+import { registerPlugins } from '@core/utils/plugins'
+
+
+import '@core/scss/template/index.scss'
+import '@layouts/styles/index.scss'
+
+const app = createApp(App)
+
+
+registerPlugins(app)
+
+app.mount('#app')
